d0aff819f921fa798feb8ffb4ce956a98625f480
donellgill <donell.gill@outlook.com>
2021-04-18 22:18:03 -0400

    can't get fetch to work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {});
+
+const showPanel = document.getElementById('show-panel')
+function getBookList(books){
+    fetch('http://localhost:3000/books')
+    .then(res => {
+        console.log(res, "Waiting to parse ...")
+        return res.json(books)
+    })
+    .then(json => {
+        console.log("Data parsed . . .", json)
+    
+    })
+    .catch(error => {
+        console.log("oh no, error!", error)
+    })
+
+}
